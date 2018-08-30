e9728a318e08c13dee278ee2d668c166a4f854c1
panza8484 <panza8484@gmail.com>
2018-08-29 20:18:54 -0700

    fix

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './App.css';
+import SectionTitle from './components/SectionTitle';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+      <SectionTitle label="SLFE CAPSTONE"/>
+      </div>
+    );
+  }
+}
+
+export default App;
