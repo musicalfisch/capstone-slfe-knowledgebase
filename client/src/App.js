89701345852b0cabe77551f801c4e36b9c1d4bce
Dickwyn <dickwyn@outlook.com>
2018-09-02 13:40:14 -0700

    added express server

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
+import Data from './components/data';
 import SectionTitle from './components/SectionTitle';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-      <SectionTitle label="SLFE CAPSTONE"/>
+        <SectionTitle label="SLFE CAPSTONE"/>
+        <Data />
       </div>
     );
   }
