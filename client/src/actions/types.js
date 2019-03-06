cd9161689f0ed6f0abb35fe94f76c32f25da76d9
George Samarripa <gsam32@gmail.com>
2019-03-06 13:37:43 -0700

    add solution basic

--- client/src/actions/types.js
+++ client/src/actions/types.js
@@ -2,6 +2,7 @@ export const GET_ENTERPRISES = 'GET_ENTERPRISES';
 export const GET_DOMAINS = 'GET_DOMAINS';
 export const GET_DOMAIN_ENTRIES = 'GET_DOMAIN_ENTRIES';
 export const GET_SOLUTIONS = 'GET_SOLUTIONS';
+export const ADD_SOLUTION = 'ADD_SOLUTION';
 export const GET_BYID = 'GET_BYID';
 export const GET_FIELD = 'GET_FIELD';
 export const RESPONSE_LOADING = 'RESPONSE_LOADING';
