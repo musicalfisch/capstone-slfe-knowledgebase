6228a94b7480d94b0a47b5a495038cb940a86654
Monsica <monica.salazar@asu.edu>
2019-03-22 21:06:03 -0700

    Improved the visuals for the login and register page. Tweaking is still needed.

--- client/src/components/PageComponents/RegisterForm/index.jsx
+++ client/src/components/PageComponents/RegisterForm/index.jsx
@@ -4,31 +4,116 @@ class RegisterForm extends Component
 {
 	render() {
 		return(
-			<div id="register_container" className="form_input">
-				<form>
-					<label htmlFor="firstname">First Name:</label>
-					<input type="text" id="first_name" name="first_name"/>
-
-					<label htmlFor="lastname">Last Name:</label>
-					<input type="text" id="last_name" name="last_name"/>
-
-					<label htmlFor="email">Email Address:</label>
-					<input type="text" id="email" name="email"/>
-
-					<label htmlFor="user">User:</label>
-					<input type="text" id="user" name="user"/>
-
-					<label htmlFor="password">Password:</label>
-					<input type="password" id="password" name="password"/>
-
-					<div id="register_bottom">
-						<input type="submit" value="register"/>
+			<div id="register_form" className="form_input">
+				<div className="signup-form">	
+					<form onSubmit={this.onSubmit}>
+						<h2>Register</h2>
+						<p className="lead">Please fill out this form to create your account.</p>
+						<div className="form-group">
+							<div className="input-group">
+								<span className="input-group-addon">
+									<i className="fa fa-user"/>
+								</span>
+								<input 
+									type="text" 
+									className="form-control" 
+									name="firstname" 
+									placeholder="First Name" 
+									required="required"
+								/>
+							</div>
+						</div>
+						<div className="form-group">
+							<div className="input-group">
+								<span className="input-group-addon">
+									<i className="fa fa-user"/>
+								</span>
+								<input 
+									type="text" 
+									className="form-control" 
+									name="lastname" 
+									placeholder="Last Name" 
+									required="required"
+								/>
+							</div>
+						</div>
+						<div className="form-group">
+							<div className="input-group">
+								<span className="input-group-addon">
+									<i className="fa fa-user"/>
+								</span>
+								<input 
+									type="text" 
+									className="form-control" 
+									name="username" 
+									placeholder="Username" 
+									required="required"
+								/>
+							</div>
+						</div>
+						<div className="form-group">
+							<div className="input-group">
+								<span className="input-group-addon">
+									<i className="fa fa-paper-plane"/>
+								</span>
+								<input 
+									type="email" 
+									className="form-control" 
+									name="email" 
+									placeholder="Email Address" 
+									required="required"
+								/>
+							</div>
+						</div>
+						<div className="form-group">
+							<div className="input-group">
+								<span className="input-group-addon">
+									<i className="fa fa-lock"/>
+								</span>
+								<input 
+									type="password" 
+									className="form-control" 
+									name="password" 
+									placeholder="Password" 
+									required="required"
+								/>
+							</div>
+						</div>
+						<div className="form-group">
+							<div className="input-group">
+								<span className="input-group-addon">
+									<i className="fa fa-lock"/>
+									<i className="fa fa-check"/>
+								</span>
+								<input 
+									type="password" 
+									className="form-control" 
+									name="confirm_password" 
+									placeholder="Confirm Password" 
+									required="required"
+								/>
+							</div>
+						</div>        
+						<div className="form-group">
+							<button 
+								type="submit" 
+								className="btn btn-primary btn-block btn-lg">
+									Register
+							</button>
+						</div>
+						<p className="small text-center">
+							By clicking the Register button, you agree to our
+							<a href="#"> Terms &amp; Conditions</a>, and <a href="#">Privacy Policy</a>.
+						</p>
+					</form>
+					<div className="text-center">
+						Already have an account? <a href="/login">Login here</a>.
 					</div>
-				</form>
-			</div>
-			
+				</div>
+			</div>	
 		);
 	}
 }
 
-export default RegisterForm
+export default RegisterForm;
+
