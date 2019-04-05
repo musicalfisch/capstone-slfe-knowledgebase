7df81568779e85c0fa5763e00c966b0e184b3f9e
Brandon Dressman <bdressma@asu.edu>
2019-04-05 13:33:56 -0700

    skeleton privacy page

--- client/src/index.js
+++ client/src/index.js
@@ -5,6 +5,7 @@ import '../node_modules/font-awesome/css/font-awesome.min.css';
 import explore from './components/Pages/explore.jsx';
 import about from './components/Pages/about';
 import terms_of_service from './components/Pages/terms_of_service';
+import privacy from './components/Pages/privacy';
 import login from './components/Pages/login';
 import register from './components/Pages/register';
 import mapContainer from './components/Pages/mapContainer.jsx';
@@ -26,6 +27,7 @@ render(
 				<Route path='/login' component={login} />
 				<Route path='/register' component={register} />
         <Route path='/terms_of_service' component={terms_of_service} />
+        <Route path='/privacy' component={privacy} />
         <Route exact path='/' component={explore}/>
         <Route path='/map' component={mapContainer} />
         <Route path='/solution/add' component={addSolution} />
