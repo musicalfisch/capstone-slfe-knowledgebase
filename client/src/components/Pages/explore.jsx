ef051a518e4682d82bd2520aa06c04a694ea4adc
George Samarripa <gsam32@gmail.com>
2019-05-08 17:02:22 -0700

    upgrade jquery version

--- client/src/components/Pages/explore.jsx
+++ client/src/components/Pages/explore.jsx
@@ -158,9 +158,9 @@ class explore extends Component {
     return (
       <Page>
         <script
-          src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
-          integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
-          crossOrigin="anonymous"
+          src="https://code.jquery.com/jquery-3.4.1.slim.min.js"
+          integrity="sha256-pasqAKBDmFT4eHoN2ndd6lN370kFiGUFyTiUHWhU7k8="
+          crossorigin="anonymous"
         />
         <script
           src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"
