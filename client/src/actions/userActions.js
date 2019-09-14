c23c5a9aa7f557a11d10d55a2e7a64bb79887b8f
George Samarripa <gsamarripa@mpb-george.local>
2019-09-14 14:23:52 -0700

    add token to header for secure endpoints

--- client/src/actions/userActions.js
+++ client/src/actions/userActions.js
@@ -40,6 +40,27 @@ export const logoutFunc = () => {
   };
 };
 
+// Setup config/headers and token
+export const tokenConfig = getState => {
+  // Get token from localstorage
+  const token = getState().auth.token;
+
+  // Headers
+  const config = {
+    headers: {
+      'Content-type': 'application/json'
+    }
+  };
+
+  // If token, add to headers
+  if (token) {
+    config.headers['x-auth-token'] = token;
+  }
+
+  return config;
+};
+
+
 export const setResponseLoading = () => {
   return{
     type: RESPONSE_LOADING
