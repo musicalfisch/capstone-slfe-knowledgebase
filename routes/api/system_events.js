28d8330fb67c68bca01992fa6f38db35840561be
George Samarripa <gsamarripa@mpb-george.local>
2019-09-30 13:32:57 -0700

    add SystemEvents & get all/post one

---
+++ routes/api/system_events.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const router = express.Router();
+
+//User Model
+const SystemEvent = require('../../models/system_event');
+
+router.get('/', (req, res) => {
+    SystemEvent.find({})
+      .then(system_events => res.json(system_events))
+  });
+
+router.post('/', (req, res) => {
+    let new_system_event = new User(req.body);
+    new_system_event.save().then(system_event => res.json(system_event));
+});
+
+module.exports = router;
