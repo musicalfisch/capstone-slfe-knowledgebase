fb53b6174589c70be3f33a5630bcec1b2cf0274d
Brandon Dressman <bdressma@asu.edu>
2019-10-06 00:33:03 -0700

    updated ordering of categories to Production, Processing, Distribution, Outlets, Recycling, Integrating per requirements

--- client/src/components/Pages/explore.jsx
+++ client/src/components/Pages/explore.jsx
@@ -44,7 +44,19 @@ class explore extends Component {
 
   getCategoryItems = () => {
     var categoryTypes = [];
+    var sorted_cat = [];
     if (this.state.domains) {
+
+      // The categories must be hard-coded order per Nigel request #190 in Taiga.
+      sorted_cat.push( this.state.domains.find( c => c.name === "Production"));
+      sorted_cat.push( this.state.domains.find( c => c.name === "Processing"));
+      sorted_cat.push( this.state.domains.find( c => c.name === "Distribution"));
+      sorted_cat.push( this.state.domains.find( c => c.name === "Outlets"));
+      sorted_cat.push( this.state.domains.find( c => c.name === "Recycling"));
+      sorted_cat.push( this.state.domains.find( c => c.name === "Integrating"));
+      this.state.domains = sorted_cat;
+
+
       for (var i = 0; i < this.state.domains.length; i++) {
         categoryTypes.push(
           <div style={{ cursor: "pointer" }}>
