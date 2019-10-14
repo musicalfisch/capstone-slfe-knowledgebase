bc635c3058e5c0695b7166abc8f53c573757f421
Brandon Dressman <bdressma@asu.edu>
2019-10-13 23:49:09 -0700

    Updated title to SLFE - Privacy Policy

--- client/src/components/Pages/privacy.jsx
+++ client/src/components/Pages/privacy.jsx
@@ -2,7 +2,7 @@ import React, { Component } from 'react'
 
 class privacy extends Component {
   componentDidMount() {
-    document.title = "Privacy Policy";
+    document.title = "SLFE - Privacy Policy";
   }
 
   render() {
